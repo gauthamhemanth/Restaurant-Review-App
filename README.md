dd7d4513f6424fe0539778d22ea19da0cfe1b8ab
hemanth <gautham.hemanth@gmail.com>
2019-09-11 12:39:42 -0400

    Version 1.0

---
+++ README.md
@@ -0,0 +1,25 @@
+
+## Project Overview: Restuarant Review App
+
+Final Project for Udacity Front-end nano Degree program. Implemented a Fully functional restaurant review application for New York downtown area. 
+
+## Features:
+- App has 2 view pages main page to list out restaurants and geolocations on map of New York downtown area.
+- Follow by a detailed page providing hours of operation and review comments by customers
+- Customer can filter the restaurant results based on the neighbourhood and cuisine of preference.
+- tab Accessibility is enabled for the an alternate navigation through out the website
+- Application is implemented in responsive design for a user to visit the website on smartphones, desktops & tablets along with offline functionality.
+
+## Instructions:
+
+- Clone the project from git Repository
+- Make sure Python is installed your local system check the version.
+- Open the terminal in project local directory and run the below command to start local server
+    - Python 2: python -m SimpleHTTPServer 8000
+    - Python 3: python3 -m http.server 8000
+- Visit http://localhost:8000 in your browser and the application should be up and running.  
+
+## Specification:
+- Browser should be ES 6 compatible
+- [leafletjs](https://leafletjs.com/) with [Mapbox](https://www.mapbox.com/) used to intergate map functionality in the App. 
+- Python should be locally installed to initiate local server.
---
+++ sw_offline_cache.js
@@ -0,0 +1,63 @@
+let CacheVersion = 'Site-v1';
+
+const CacheContentArray = [
+
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './css/responsive_styles.css',
+    './data/restaurants.json',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg'
+
+];
+
+/* add web pages to local cache storage */
+self.addEventListener('install',(e) => {
+   console.log('Service Worker: Installed');
+
+   e.waitUntil(
+       caches
+           .open(CacheVersion)
+           .then(cache => cache.addAll(CacheContentArray)
+           ).then(() => self.skipWaiting())
+
+   );
+
+});
+
+/* maintain only one copy of storage*/
+self.addEventListener('activate',(e) => {
+    console.log('Service Worker: Activated');
+    e.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cache => {
+                        if(cache !== CacheVersion){
+                            return caches.delete(cache);
+                        }
+                    })
+                );
+            })
+    );
+});
+
+/* offline mode only : Fetch pages from cache storage*/
+self.addEventListener('fetch', (e) =>{
+    e.respondWith(
+        fetch(e.request)
+            .catch(() => caches.match(e.request))
+    );
+});
